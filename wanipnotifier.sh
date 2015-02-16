4467025c062fbc4573ebf54b29111c707854e71b
Rehaell <Rehaell@users.noreply.github.com>
2015-02-16 16:00:26 +0400

    Update wanipnotifier.sh

--- wanipnotifier.sh
+++ wanipnotifier.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# create a file called wan_ip-cf.txt on /tmp/
 # info for cloudflare service 
 cfkey=KEY
 cfuser=USER
@@ -13,7 +13,7 @@ WAN_IP=`curl -s http://icanhazip.com`
 
 if [ -f wan_ip-cf.txt ]; 
 then
-        OLD_WAN_IP=`cat /home/pi/myscripts/wan_ip-cf.txt` 
+        OLD_WAN_IP=`cat /tmp/wan_ip-cf.txt` 
 else
         echo "No file, need IP"
         OLD_WAN_IP="" 
@@ -24,7 +24,7 @@ then
         echo "IP Unchanged"
         echo "WAN IP is $WAN_IP" 
 else
-        echo $WAN_IP > /home/pi/myscripts/wan_ip-cf.txt
+        echo $WAN_IP > /tmp/wan_ip-cf.txt
         echo "Updating DNS to $WAN_IP"
         curl -s https://www.cloudflare.com/api.html?a=DIUP\&hosts="$cfhost"\&u="$cfuser"\&tkn="$cfkey"\&ip="$WAN_IP" > /dev/null
 
